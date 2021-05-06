e3e602ecbebe4c117e4a350dc084dbf8b06af3ed
Megan Schmidt <schmidtm6@hawkmail.newpaltz.edu>
2021-05-05 20:05:19 -0400

    Install express, graphql

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const {graphqlHTTP} = require('express-graphql'); //Name does not equal package for thi
+
+const app = express(); //create app
+
+//Middleware
+app.use('/graphql', graphqlHTTP({
+
+})) //everything to this path is handled by graphqlhttp
+
+app.listen(4000,() => {
+    console.log('Now listening for requests on port 4000');
+}) // listen to this port
